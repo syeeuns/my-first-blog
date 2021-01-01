1496577e3c694f692c79f85c65045ad78ac5f59b
syeeuns <syeeuns@gmail.com>
2021-01-01 23:58:10 +0900

    please..

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.htm', {'posts':posts})
@@ -9,4 +10,4 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.htm', {'post':post})
+    return render(request, 'blog/post_detail.htm', {'post': post})
