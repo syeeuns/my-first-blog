cefeba1a46bf96026ba14f15fa68525464b0c39e
syeeuns <syeeuns@gmail.com>
2021-01-01 15:26:32 +0900

    BASE_DIR mod

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os, json
 from django.core.exceptions import ImproperlyConfigured #예외 처리를 위해 불러와줍니다.
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 secret_file = os.path.join(BASE_DIR, 'secrets.json') #secrets.json을 불러와 줍니다.
 
 with open(secret_file, 'r') as f: #open as로 secret.json을 열어줍니다.
@@ -26,10 +29,6 @@ def get_secret(setting, secrets=secrets): #예외 처리를 통해 오류 발생
         raise ImproperlyConfigured(error_msg)
 
 SECRET_KEY = get_secret("SECRET_KEY")
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
